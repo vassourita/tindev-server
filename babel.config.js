0183ac6d881bd6282c3b72842e2b8ec111d6cf5d
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-07-13 16:31:42 -0300

    Add babel

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@config/*': './src/config/*',
+          '@modules/*': './src/modules/*',
+          '@shared/*': './src/shared/*',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.spec.ts'],
+};
